fed55e0b9bbd67575134a570ae2ea7c14125efdc
hikaricp <dockerc@163.com>
2023-05-16 21:19:17 +0800

    lesson-11

--- app.js
+++ app.js
@@ -2,29 +2,16 @@ new Vue({
   el: '#vue-app',
   data() {
     return {
-      a: 0,
-      b: 0,
-      age: 23
+      myChangeColor: false,
+      myChangeLength: false
     }
   },
-  methods: {
-    // addToA() {
-    //   console.log("addAgeToA");
-    //   return this.a + this.age;
-    // },
-    // addToB() {
-    //   console.log("addAgeToB");
-    //   return this.b + this.age;
-    // }
-  },
   computed: {
-    addToA() {
-      console.log("addAgeToA");
-      return this.a + this.age;
-    },
-    addToB() {
-      console.log("addAgeToB");
-      return this.b + this.age;
+    compClasses() {
+      return {
+        changeColor: this.myChangeColor,
+        changeLength: this.myChangeLength
+      }
     }
   }
 })
