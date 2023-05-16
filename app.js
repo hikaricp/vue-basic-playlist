d3c48b5eb8a5138069f451d973e5c5792d445a37
hikaricp <dockerc@163.com>
2023-05-16 22:58:35 +0800

    lesson-12

--- app.js
+++ app.js
@@ -2,16 +2,8 @@ new Vue({
   el: '#vue-app',
   data() {
     return {
-      myChangeColor: false,
-      myChangeLength: false
-    }
-  },
-  computed: {
-    compClasses() {
-      return {
-        changeColor: this.myChangeColor,
-        changeLength: this.myChangeLength
-      }
+      error: false,
+      success: false
     }
   }
 })
