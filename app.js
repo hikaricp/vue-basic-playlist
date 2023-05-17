6e76c75da65f6ed78cbf6bcce1bfab31869b6368
hikaricp <dockerc@163.com>
2023-05-17 22:59:15 +0800

    lesson-15

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
-new Vue({
-  el: '#vue-app',
+const one = new Vue({
+  el: '#vue-app-one',
   data() {
     return {
-      health: 100,
-      ended: false
+      title: 'this is app one'
+    }
+  },
+  computed: {
+    greet() {
+      return 'Hello App one'
+    }
+  }
+});
+
+const two = new Vue({
+  el: '#vue-app-two',
+  data() {
+    return {
+      title: 'this is app two'
     }
   },
   methods: {
-    punch() {
-      this.health -= 10;
-      if (this.health <= 0) {
-        this.ended = true;
-      }
-    },
-    restart() {
-      this.health = 100;
-      this.ended = false;
+    changeAppOneTitle() {
+      one.title = '这是 APP one 的 title';
+    }
+  },
+  computed: {
+    greet() {
+      return 'Hello App two'
     }
   }
-})
+});
+
+two.title = '这是 APP two 的 title'
