3b52fd0b62c3f3e82ddd4e4088c74131cd4109e9
hikaricp <dockerc@163.com>
2023-05-18 06:54:11 +0800

    lesson-16

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
+let data = {
+  name: 'James',
+  wechat: 'james666'
+};
+
+Vue.component('Greeting', {
+  template: `
+    <p>
+      我是：{{name}}, 我的微信是：{{wechat}}
+      <button @click="changeName">改名</button>
+    </p>
+  `,
+  data() {
+    return data;
+  },
+  methods: {
+    changeName() {
+      this.name = 'KB'
+    }
+  }
+});
 const one = new Vue({
   el: '#vue-app-one',
   data() {
-    return {
-      title: 'this is app one'
-    }
-  },
-  computed: {
-    greet() {
-      return 'Hello App one'
-    }
+    return {}
   }
 });
 
 const two = new Vue({
   el: '#vue-app-two',
   data() {
-    return {
-      title: 'this is app two'
-    }
-  },
-  methods: {
-    changeAppOneTitle() {
-      one.title = '这是 APP one 的 title';
-    }
-  },
-  computed: {
-    greet() {
-      return 'Hello App two'
-    }
+    return {}
   }
 });
-
-two.title = '这是 APP two 的 title'
