89e0ad09dd906c928e65f96515155a89822d5ed3
hikaricp <dockerc@163.com>
2023-05-18 11:48:08 +0800

    lesson-18 axios

--- app.js
+++ app.js
@@ -10,28 +10,15 @@ new Vue({
     }
   },
   mounted() {
-    fetch('http://jsonplaceholder.typicode.com/todos')
-      .then(res => {
-        return res.json();
-      }).then(todos => {
-        this.todos = todos;
-      });
+    axios.get('http://jsonplaceholder.typicode.com/todos')
+      .then(res => this.todos = res.data);
   },
   methods: {
     onSubmit() {
-      fetch('http://jsonplaceholder.typicode.com/todos', {
-        method: 'POST',
-        body: JSON.stringify(this.todo),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
+      axios.post('http://jsonplaceholder.typicode.com/todos', this.todo)
         .then(res => {
-          return res.json();
-        })
-        .then(todo => {
-          // console.log(todo);
-          this.todos.unshift(todo);
+          // console.log(res.data);
+          this.todos.unshift(res.data);
         })
     }
   }
